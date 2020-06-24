76d805deb33c747264664b6b70ae502be19cf678
Ethan Jones <ethan.jones@bbconsult.co.uk>
2020-06-24 13:59:38 +0500

    #213551n Adding gateway

--- Handler.cs
+++ Handler.cs
@@ -1,31 +1,32 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response Hello(Request request)
-       {
-           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
-       }
-    }
+       public APIGatewayProxyResponse Hello(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            try{
+                context.Logger.LogLine("START");
+                
 
-    public class Response
-    {
-      public string Message {get; set;}
-      public Request Request {get; set;}
-
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
-      }
-    }
-
-    public class Request
-    {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
+                context.Logger.LogLine("STOP");
+            }
+            catch(Exception ex)
+            {
+                context.Logger.LogLine(ex.Message);
+            }
+            return new APIGatewayProxyResponse()
+            {
+                StatusCode = 200,
+                Body = JsonConvert.SerializeObject(new { msg = "Hello World!" }),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }                
+            };
+        }
     }
 }
